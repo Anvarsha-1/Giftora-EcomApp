9530b4a4fd34c1de242dfa33d099717a4f6aaa05
Anvarsha-1 <anvarshaanvar576@gmail.com>
2025-06-16 01:17:50 +0530

    Signup page,Form validation , Otp transport

--- routers/user/userRouter.js
+++ routers/user/userRouter.js
@@ -4,6 +4,8 @@ const userController = require("../../controllers/user/userController")
 
 router.get('/',userController.loadHomePage);
 router.get('/PageNotFound',userController.PageNotFound);
+router.get("/signup",userController.loadSignUp);
+router.post("/signup",userController.signUp);
 
 
 module.exports=router
