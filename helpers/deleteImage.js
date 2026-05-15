efc884b7ac2460c51fc86164c97af7aa96bdae1e
Anvarsha <anvarshaanvar576@gmail.com>
2026-05-15 09:34:24 +0530

    Fix syntax error in deleteUploadedImages function

--- helpers/deleteImage.js
+++ helpers/deleteImage.js
@@ -3,7 +3,7 @@ const cloudinary = require('cloudinary').v2;
 const deleteUploadedImages = async (files) => {
   for (const file of files || []) {
     try {
-      const publicId = file.filename.split('.')[0]; // Adjust if needed
+      const publicId = file.filename.split('.')[0]; 
       await cloudinary.uploader.destroy(publicId);
     } catch (err) {
       console.error('Cloudinary deletion failed:', err.message);
